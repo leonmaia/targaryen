1b3ee7967ba1636a45dc99251f74c1646a641b30
Leon Maia <leon.maia@hp.com>
2014-11-14 13:06:39 -0300

    user validation on journey

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,4 +1,6 @@
 class Journey < ActiveRecord::Base
   belongs_to :user
   has_many :locations
+
+  validates_presence_of :user_id
 end
--- spec/models/journey_spec.rb
+++ spec/models/journey_spec.rb
@@ -4,22 +4,30 @@ require 'database_cleaner'
 DatabaseCleaner.strategy = :truncation
 
 RSpec.describe Journey, :type => :model do
+  describe Journey do
+    it { should belong_to(:user) }
+    it { should validate_presence_of(:user_id) }
+  end
+
   describe 'locations association' do
-    before(:all) do
+    before(:each) do
       DatabaseCleaner.clean
     end
 
     it 'should have journey_id setted on location' do
-      @journey = Journey.create!
+      @user = User.create!
+      @journey = @user.journeys.create!
       @journey.locations.create!
 
       expect(Journey.all.first.locations.first.journey_id).to eq(@journey.id)
     end
 
     it 'can have many locations' do
-      @journey = Journey.create!
+      @user = User.create!
+      @journey = @user.journeys.create!
       50.times { @journey.locations.create! }
 
+      expect(Journey.all.count).to eq(1)
       expect(Journey.all.first.locations.count).to eq(50)
     end
   end
